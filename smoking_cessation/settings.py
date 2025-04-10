1e975fa9e392299c09ea63fdf60383cd22477a7e
sajadaoud1 <dawoodsaja9@gmail.com>
2025-04-11 01:17:08 +0300

    Add push notification feature

--- smoking_cessation/settings.py
+++ smoking_cessation/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
     'allauth',
     'allauth.account',
@@ -143,3 +144,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ["*", "testserver"]
